b91acc01a7ed966d8c681e42e887d2faaa48c5f2
ICI-INC <166365607+ICI-INC@users.noreply.github.com>
2025-03-05 17:49:02 -0300

    Add files via upload

---
+++ dados/dados.js
@@ -0,0 +1,15 @@
+function logar(){
+
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login = "1" && senha == "37254459"){ //Marcelo
+        alert('Sucesso');
+        location.href = "https://ici-inc.github.io/icitv/assets/icitv.html"; 
+    }else{
+        
+    }
+
+    
+            
+}
