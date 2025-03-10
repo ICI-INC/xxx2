94137727e5d3e4e3e4bcdfd5dcca740889cc589f
ICI-INC <166365607+ICI-INC@users.noreply.github.com>
2025-03-10 15:31:24 -0300

    Update dados.js

--- dados/dados.js
+++ dados/dados.js
@@ -4,12 +4,11 @@ function logar(){
     var senha = document.getElementById('senha').value;
 
     if(login = "1" && senha == "1"){ //Marcelo
+      (login = "rambo" && senha == "3"){ //rambo
         alert('Sucesso');
         location.href = "https://ici-inc.github.io/novidades/icitv.html"; 
     }else{
         
     }
-
-    
             
 }
